b3c281e09b54ae5db1bbdc31b4fe4770572e90bb
LucasPetruci <lucaspetruci100@gmail.com>
2024-08-08 16:15:30 -0300

    chore: enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurando CORS
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
